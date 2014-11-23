f3ff9073751b2a3a6a6e5b8255b63d07dc3a02e9
Will Venn <infohack3@gmail.com>
2014-11-23 22:26:49 +0000

    Added server downloader

---
+++ Downloader.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+#A simple application that downloads the latest minecraft vanilla server packages and files
+
+import urllib2
+
+url = "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
+
+file_name = url.split('/')[-1]
+u = urllib2.urlopen(url)
+f = open(file_name, 'wb')
+meta = u.info()
+file_size = int(meta.getheaders("Content-Length")[0])
+print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+file_size_dl = 0
+block_sz = 8192
+while True:
+    buffer = u.read(block_sz)
+    if not buffer:
+        break
+
+    file_size_dl += len(buffer)
+    f.write(buffer)
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = status + chr(8)*(len(status)+1)
+    print status,
+
+f.close()
+
