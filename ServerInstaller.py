657dde04044e07f171a0f75482a1625d36c5eecb
Will Venn <infohack3@gmail.com>
2014-11-23 21:54:44 +0000

    fixed import bugs

--- ServerInstaller.py
+++ ServerInstaller.py
@@ -12,17 +12,13 @@ import subprocess
 #----------------------------------------
 
 root = Tk() #Creates the window called 'Root'
-root.geometry("800x600+250+450") #Sets the size of the 
-window and the position
-root.title("Minecraft Server Installer") #Sets the title 
-of the root window
+root.geometry("800x600+250+450") #Sets the size of the window and the position
+root.title("Minecraft Server Installer") #Sets the title of the root window
 #----------------------------------------
 
 #Labels
-label_welcome = Label(text="Welcome to Will's Vanilla 
-Minecraft server installer").pack()
-label_warning = Label(text="MAKE SURE YOU HAVE JAVA AND 
-JKD OR OPENJDK INSTALLED", fg="red").pack()
+label_welcome = Label(text="Welcome to Will's Vanilla Minecraft server installer").pack()
+label_warning = Label(text="MAKE SURE YOU HAVE JAVA AND JKD OR OPENJDK INSTALLED", fg="red").pack()
 breakline_label = Label(text="__________________________________________________________________________________________________________").pack()
 #----------------------------------------
 
