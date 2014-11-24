51964bd43eb1283d5fcab285d8dfdfad0420b65c
Will Venn <infohack3@gmail.com>
2014-11-24 18:08:39 +0000

    Added icon to app

--- DownloadFinished.py
+++ DownloadFinished.py
@@ -5,4 +5,10 @@ from tkinter import *
 root = Tk()
 root.title("Download finished!")
 label_1 = Label(text="Server files have been downloaded in your current working directory").pack()
+
+photo = PhotoImage(file="Images/downloadicon.jpg")
+logo = Label(root, image=photo)
+logo.photo = photo
+logo.pack()
+
 root.mainloop()
