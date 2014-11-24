6e86b123562657440982ef428c1b8a6feb541fc8
Will Venn <infohack3@gmail.com>
2014-11-24 17:58:38 +0000

    Shows a window displaying when download finished

---
+++ DownloadFinished.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.4
+
+from tkinter import *
+
+root = Tk()
+root.title("Download finished!")
+label_1 = Label(text="Server files have been downloaded in your current working directory").pack()
+root.mainloop()
