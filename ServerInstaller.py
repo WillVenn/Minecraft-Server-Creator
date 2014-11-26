43e7c072f89cdf26d24af66a75185c2f36ca0a8f
Will Venn <infohack3@gmail.com>
2014-11-26 16:32:43 +0000

    Added RAM specific auto-generation of launch scripts

--- ServerInstaller.py
+++ ServerInstaller.py
@@ -12,6 +12,51 @@ import urllib
 def jarDownload():
     subprocess.call(['python2.7', 'Downloader.py'])
     print ("Downloading server packages...")
+
+def RAM_ONE():
+	print ("Generating launch scripts for 512mb RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx512M -Xms512M -jar minecraft_server.jar nogui")
+    print ("Generated 512mb server launch scripts")
+    RAM_ONE.close()
+
+def RAM_TWO():
+	print ("Generating launch scripts for 1GB RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui")
+    print ("Generated 1GB server launch scripts")
+    RAM_ONE.close()
+
+def RAM_THREE():
+	print ("Generating launch scripts for 2GB RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx2048 -Xms2048M -jar minecraft_server.jar nogui")
+    print ("Generated 2GB server launch scripts")
+    RAM_ONE.close()
+
+def RAM_FOUR():
+	print ("Generating launch scripts for 3GB RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx3072  -Xms3072 -jar minecraft_server.jar nogui")
+    print ("Generated 3GB server launch scripts")
+    RAM_ONE.close()
+
+def RAM_FIVE():
+	print ("Generating launch scripts for 4GB RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx4096  -Xms4096 -jar minecraft_server.jar nogui")
+    print ("Generated 4GB server launch scripts")
+    RAM_ONE.close()
+
+def RAM_SIX():
+	print ("Generating launch scripts for 5GB RAM")
+    RAM_ONE = open("launch.sh", "w+")
+    RAM_ONE.write("java -Xmx5120  -Xms5120 -jar minecraft_server.jar nogui")
+    print ("Generated 5GB server launch scripts")
+    RAM_ONE.close()
+
+
+
 #----------------------------------------
 
 root = Tk() #Creates the window called 'Root'
@@ -26,7 +71,7 @@ breakline_label = Label(text="__________________________________________________
 #----------------------------------------
 
 #Buttons
-button_download = Button(text="Download server packages", command=jarDownload).pack()
+button_download = Button(text="1.) Download server packages", command=jarDownload).pack()
 #----------------------------------------
 
 #Images
@@ -35,6 +80,18 @@ logo = Label(root, image=photo)
 logo.photo = photo
 logo.pack()
 #----------------------------------------
+
+
+breakline_label2 = Label(text="__________________________________________________________________________________________________________").pack()
+
+label_chooseram = Label(text="2.) Once the server files have been downloaded choose how much RAM you have/would like to allocate for the server").pack()
+
+button_512 = Button(text="512MB", bd=2, command=RAM_ONE).pack()
+button_1024 = Button(text="1GB", bd=2, command=RAM_TWO).pack()
+button_2048 = Button(text="2GB", bd=2, command=RAM_THREE).pack()
+button_3072 = Button(text="3GB", bd=2, command=RAM_FOUR).pack()
+button_4096 = Button(text="4GB", bd=2, command=RAM_FIVE).pack()
+button_5120= Button(text="5GB", bd=2, command=RAM_SIX).pack()
 
 
 
