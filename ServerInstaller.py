a37abafaefa1eedda543e2d23c314eeb0be2b103
Will Venn <infohack3@gmail.com>
2014-11-27 21:51:24 +0000

    Deleted DownloadIcon and added to ServerInstaller

--- ServerInstaller.py
+++ ServerInstaller.py
@@ -22,45 +22,51 @@ def start():
 	subprocess.call(['sh', 'launch.sh'])
 	print ("Server started...")
 
+def about():
+	pass
+	##subprocess.call(['python', 'Extra/About.py'])
+	##print ("About screen launched")
+
+
 def RAM_ONE():
 	print ("Generating launch scripts for 512mb RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx512M -Xms512M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx512M -Xms512M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 512mb server launch scripts")
 	RAM_ONE.close()
 
 def RAM_TWO():
 	print ("Generating launch scripts for 1GB RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 1GB server launch scripts")
 	RAM_ONE.close()
 
 def RAM_THREE():
 	print ("Generating launch scripts for 2GB RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx2048M -Xms2048M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx2048M -Xms2048M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 2GB server launch scripts")
 	RAM_ONE.close()
 
 def RAM_FOUR():
 	print ("Generating launch scripts for 3GB RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx3072M  -Xms3072M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx3072M  -Xms3072M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 3GB server launch scripts")
 	RAM_ONE.close()
 
 def RAM_FIVE():
 	print ("Generating launch scripts for 4GB RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx4096M  -Xms4096M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx4096M  -Xms4096M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 4GB server launch scripts")
 	RAM_ONE.close()
 
 def RAM_SIX():
 	print ("Generating launch scripts for 5GB RAM")
 	RAM_ONE = open("launch.sh", "w+")
-	RAM_ONE.write("java -Xmx5120M  -Xms5120M -jar minecraft_server.1.8.jar nogui")
+	RAM_ONE.write("java -Xmx5120M  -Xms5120M -jar minecraft_server.1.8.1.jar nogui")
 	print ("Generated 5GB server launch scripts")
 	RAM_ONE.close()
 
@@ -71,6 +77,13 @@ def RAM_SIX():
 root = Tk() #Creates the window called 'Root'
 root.geometry("800x600+250+450") #Sets the size of the window and the position
 root.title("Minecraft Server Installer") #Sets the title of the root window
+#----------------------------------------
+
+#Menubar
+menubar = Menu(root)
+menubar.add_command(label="About", command=about)
+menubar.add_command(label="Quit", command=root.quit)
+root.config(menu=menubar)
 #----------------------------------------
 
 #Labels
@@ -90,7 +103,8 @@ logo.photo = photo
 logo.pack()
 #----------------------------------------
 
-
+#----------------------------------------
+#RAM selection
 breakline_label2 = Label(text="__________________________________________________________________________________________________________").pack()
 
 label_chooseram = Label(text="2.) Once the server files have been downloaded choose how much RAM you have/would like to allocate for the server").pack()
@@ -102,13 +116,14 @@ button_3072 = Button(text="3GB", bd=2, command=RAM_FOUR).pack()
 button_4096 = Button(text="4GB", bd=2, command=RAM_FIVE).pack()
 button_5120= Button(text="5GB", bd=2, command=RAM_SIX).pack()
 label_more = Label(text="Support for more RAM soon!").pack()
-
+#----------------------------------------
 breakline_label3 = Label(text="__________________________________________________________________________________________________________").pack()
-
+#Permssion stuffs
 label_chmod = Label(text="2.) Now we must make it exactuable...").pack()
 button_chmod = Button(text="Run permission script", bd=2, command=perm).pack()
-
+#---------------------------------------
 breakline_label4 = Label(text="__________________________________________________________________________________________________________").pack()
+#Server starting shizzle
 label_Start = Label(text="Congratulations!").pack()
 button_Start = Button(text="Press me to start your newly installed Vanilla Minecraft server!", bd=3, command=start).pack()
 
